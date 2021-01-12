f8748a83659b1f775b96cd650bdabdf65f313915
Török Attila <torcsiattila93@gmail.com>
2021-01-12 20:58:50 +0100

    feat(signup): email verification is added to the sign up page

--- controller/routes.js
+++ controller/routes.js
@@ -1,3 +1,15 @@
+const nodemailer = require('nodemailer');
+
+var smtpTransport = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'personalgallery12@gmail.com',
+        pass: 'mypersonalgallery1'
+    }
+});
+
+var randomNumber, mailOptions, host, link;
+
 module.exports = (app) => {
     app.get('/', (req, res) => {
         res.render('login.ejs');
@@ -5,5 +17,41 @@ module.exports = (app) => {
 
     app.get('/signup', (req, res) => {
         res.render('signup.ejs');
+    });
+
+    app.post('/send', (req, res) => {
+        randomNumber = Math.floor((Math.random() * 1000000000) + 54);
+        host = req.get('host');
+        link = 'http://' + req.get('host') + '/verify?id=' + randomNumber;
+
+        mailOptions = {
+            to: req.body.signup_email,
+            subject: 'Confirm your Email address',
+            html: '<h3>Click the link down below to confirm your email address!</h3><br><a href="' + link + '">' + link + '</a>'
+        }
+
+        smtpTransport.sendMail(mailOptions, (error, response) => {
+            if (error) {
+                res.render('emailsenderror.ejs');
+            } else {
+                res.render('emailsend.ejs');
+            }
+        })
+    });
+
+    app.get('/verify', (req, res) => {
+        // console.log(req.protocol + ':/' + req.get('host'));
+
+        if ((req.protocol + '://' + req.get('host')) == ('http://' + host)) {
+
+            if (req.query.id == randomNumber) {
+
+                res.render('verifypage.ejs');
+            } else {
+                res.end("<h1>Bad Request</h1>");
+            }
+        } else {
+            res.end("<h1>Request is from unknown source!</h1>");
+        }
     });
 }
